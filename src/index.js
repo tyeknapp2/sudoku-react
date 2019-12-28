cee61f6d6939a8037636d5413210e23986ac813d
Tye Knappenberger <tyeknapp.pro@gmail.com>
2019-12-27 19:55:00 -0500

    initialtest

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  render() {
+    return <div>H</div>;
+  }
+}
+
+ReactDOM.render(<Game />, document.getElementById("root"));
